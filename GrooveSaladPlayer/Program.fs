910aab49b2e8e770f5a3ccccb6edf00558c75bfb
Hans Isbrücker <hisbrucker@gmail.com>
2024-09-12 12:27:31 +0200

    Add project files.

---
+++ GrooveSaladPlayer/Program.fs
@@ -0,0 +1,25 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open NAudio.MediaFoundation
+open NAudio.Wasapi
+open NAudio.Wave
+open System.Threading
+
+
+let url = "https://ice1.somafm.com/groovesalad-128-aac"
+
+printfn "Starting Groove Salad Playback"
+
+let start () =
+    use mf = new MediaFoundationReader(url)
+    use wo = new WasapiOut()
+
+    wo.Init(mf)
+    wo.Play()
+
+    while true do
+        Thread.Sleep 1000
+
+
+start()
+
+
